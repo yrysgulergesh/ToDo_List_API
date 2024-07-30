4c7ad51904eb796024eafb928a557a8a1830d511
yrysgulergesh <yrysgulergesh@gmai.com>
2024-07-31 00:03:59 +0600

    add crud.py

---
+++ app/crud.py
@@ -0,0 +1,30 @@
+# app/crud.py
+from sqlalchemy.orm import Session
+from app import models, schemas
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    db_user = models.User(username=user.username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_tasks(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Task).offset(skip).limit(limit).all()
+
+def create_task(db: Session, task: schemas.TaskCreate, user_id: int):
+    db_task = models.Task(**task.dict(), owner_id=user_id)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
